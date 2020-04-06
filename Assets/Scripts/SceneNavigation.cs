9789cdacba50f0178e79384c4c1bac16151f6840
rouxg <guill.roux@hotmail.fr>
2020-04-06 17:08:46 -0400

    scenes nav

---
+++ Assets/Scripts/SceneNavigation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigation : MonoBehaviour
+{
+    public void LoadScene(int id)
+    {
+        SceneManager.LoadScene(id);
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}
