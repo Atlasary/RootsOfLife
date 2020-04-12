2b77fc38d2d25548dd93db13bb43f1680889c809
unknown <loic.mas1@uqac.ca>
2020-04-12 22:16:27 +0200

    Loic

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string ToLoadLevel = "MainScene";
+	public string ToLoadSucces = "MainScene";
+	public string ToLoadGlossaire = "MainScene";
+	public string ToLoadOption = "MainScene";
+    public SceneFader sceneFader;
+
+    public void SelectLevel()
+    {
+        sceneFader.FadeTo(ToLoadLevel);
+    }
+	
+	public void SelectSucces()
+    {
+        sceneFader.FadeTo(ToLoadSucces);
+    }
+	
+	public void SelectGlossaire()
+    {
+        sceneFader.FadeTo(ToLoadGlossaire);
+    }
+	
+	public void SelectOption()
+    {
+        sceneFader.FadeTo(ToLoadOption);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Fermeture du jeu");
+        Application.Quit();
+    }
+
+}
