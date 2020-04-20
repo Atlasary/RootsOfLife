96a8952a1b22307c8a31185397ea64b3d82100df
unknown <loic.mas1@uqac.ca>
2020-04-21 00:35:34 +0200

    objective

---
+++ Assets/Scripts/Objective.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Objective : MonoBehaviour{
+	
+	public GameObject manager;
+	public Text Objective1Text;
+	public bool wave = false;
+	public Text Objective2Text;
+	
+	
+	void Update () {
+		if (manager.GetComponent<PlayerStats>().startMoney != PlayerStats.money){
+			Objective1Text.text = "Achieved";
+		}
+		if (WaveSpawner.EnemiesAlive!=0 && wave==false){
+			wave=true;
+		}
+		if (WaveSpawner.EnemiesAlive == 0 && wave==true){
+			Objective2Text.text = "Achieved";
+		}
+		
+	}
+
+}
