c603599abf7006d140fcff60bbc4b9ca25d39db1
unknown <loic.mas1@uqac.ca>
2020-04-20 22:22:43 +0200

    overlay update

---
+++ Assets/Scripts/OptionGame.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionGame : MonoBehaviour {
+
+    public GameObject ui;
+
+    public SceneFader sceneFader;
+
+    public void Toggle()
+    {
+        ui.SetActive(!ui.activeSelf);
+
+        if (ui.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+
+    }
+
+}
